981907ebc6bd7fe6a96fb7d76050698f090d2955
moawasthi <31694684+moawasthi@users.noreply.github.com>
2018-12-02 18:48:07 +0530

    full stop and documentation code

--- script.js
+++ script.js
@@ -20,7 +20,48 @@ var noteContent = '';
 var notes = getAllNotes();
 renderNotes(notes);
 
+function Export2Doc(element, filename = ''){
+  var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+  var postHtml = "</body></html>";
 
+  
+  var elementN = document.getElementById(element).value;
+  var formatted = replaceAll(elementN, "@", "<br/>");
+  formatted = replaceAll(formatted, "\\." , "&#x964");
+
+
+  var html = preHtml+formatted+postHtml;
+
+  var blob = new Blob(['\ufeff', html], {
+      type: 'application/msword'
+  });
+  
+  // Specify link url
+  var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+  
+  // Specify file name
+  filename = filename?filename+'.doc':'document.doc';
+  
+  // Create download link element
+  var downloadLink = document.createElement("a");
+
+  document.body.appendChild(downloadLink);
+  
+  if(navigator.msSaveOrOpenBlob ){
+      navigator.msSaveOrOpenBlob(blob, filename);
+  }else{
+      // Create a link to the file
+      downloadLink.href = url;
+      
+      // Setting the file name
+      downloadLink.download = filename;
+      
+      //triggering the function
+      downloadLink.click();
+  }
+  
+  document.body.removeChild(downloadLink);
+}
 
 /*-----------------------------
       Voice Recognition 
@@ -129,7 +170,9 @@ notesList.on('click', function(e) {
     target.closest('.note').remove();
   }
 });
-
+/*
+Export functionality
+*/
 
 
 /*-----------------------------
@@ -199,5 +242,9 @@ function getAllNotes() {
 
 function deleteNote(dateTime) {
   localStorage.removeItem('note-' + dateTime); 
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
 }
 
